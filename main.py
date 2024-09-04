6da5cac5549232c5d09df22e4c64222591dd7e19
MrWayne-007 <eg183284@engug.ruh.ac.lk>
2024-09-04 18:26:42 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import random 
+word_list=["camel","elephant","dog"]
+
+chosen_word=random.choice(word_list)
+print(chosen_word)
+
+guess = input("guess a letter : ").lower()
+print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else:
+        print("wrong")
+        
+
+
