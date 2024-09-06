23aa6e0714b841afd593ee6b477ba4423193243d
MrWayne-007 <eg183284@engug.ruh.ac.lk>
2024-09-06 18:43:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,36 @@ word_list=["camel","elephant","dog"]
 chosen_word=random.choice(word_list)
 print(chosen_word)
 
-guess = input("guess a letter : ").lower()
-print(guess)
+placeholder = ""
+word_length = len(chosen_word)
 
-for letter in chosen_word:
-    if letter == guess:
-        print("right")
-    else:
-        print("wrong")
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+game_over= False
+correct_letters = []
+
+while not game_over:
+    guess = input("guess a letter : ").lower()
+    print(guess)
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over=True
+        print("you won")
+
 
 # Work in progress
         
